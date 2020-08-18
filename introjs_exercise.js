8bc1f64e4f88204d2d8b20f46d6145bbc39dee9b
ZainalFauzi <fourth.breast@gmail.com>
2020-08-18 20:16:01 +0700

    tugas javascript introduction

---
+++ introjs_exercise.js
@@ -0,0 +1,47 @@
+//soal nomor 1
+console.log("Zainal Fauzi");
+console.log("Malang");
+console.log("7 Mei 1995");
+console.log(25);
+console.log("Q-HSE Staff");
+
+//soal nomor 2
+let namaUser = prompt("Masukkan nama anda");
+let asalNegara = prompt("Masukkan negara asal anda");
+
+confirm("Apakah data yang diisi sudah benar?");
+alert("Terima kasih sudah mengisi form");
+
+//soal nomor 3
+//console.log("Zainal Fauzi");
+//console.log("Malang");
+//console.log("7 Mei 1995");
+//console.log(25);
+//console.log("Q-HSE Staff");
+
+//soal nomor 4
+/*let namaUser = prompt("Masukkan nama anda");
+let asalDaerah = prompt("Masukkan negara asal anda");
+
+confirm("Apakah data yang diisi sudah benar?");
+alert("Terima kasih sudah mengisi form"); */
+
+//soal nomor 5
+let dataDiri = {
+  nama: "Zainal Fauzi",
+  asalKota: "Malang",
+  tglLahir: "7 Mei 1995",
+  umur: 25,
+  posisiPekerjaan: "Q-HSE Staff",
+};
+console.log(dataDiri);
+
+//soal nomor 6
+let tambah = 25 + 10;
+let kali = 100 * 2;
+let bagi = 99 / 2;
+let mod = 99 % 2;
+console.log(tambah);
+console.log(kali);
+console.log(bagi);
+console.log(mod);
